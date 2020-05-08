9dacac30a41cb9c4160703e75097f42191e21a3d
don-k-jacob <donkjacob.business@gmail.com>
2020-05-09 02:16:58 +0530

    first commit

---
+++ app/src/main/java/com/example/hellowworld/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.hellowworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    lateinit var imgView: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{
+            DiscRolled()
+        }
+        imgView=findViewById(R.id.dice_img)
+    }
+
+    private fun DiscRolled() {
+
+        val randomInt=Random.nextInt(6)+1
+        val drowableResult = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
+
+        imgView.setImageResource(drowableResult)
+    }
+}
